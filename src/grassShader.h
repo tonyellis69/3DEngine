fc53d17dc43b99abff7aa7f9bd85576ae4382221
tonyellis69 <tonyellis69@gmail.com>
2017-08-11 16:09:27 +0100

    Grass now waves

--- src/grassShader.h
+++ src/grassShader.h
@@ -13,8 +13,10 @@ public:
 	void getShaderHandles();
 	void setMVP(glm::mat4& matrix);
 	void setTextureUnit(unsigned int textureUnit);
+	void setTime(float time);
 
 
 	GLuint hMVP; ///<Handle to standard texture shader MVP matrix
 	GLuint hTexure; 
+	GLuint hTime;
 };
